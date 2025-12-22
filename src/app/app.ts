97cfaaa6ead2c5445782ef26beb8cb0c565cda38
K.Ashok Kumar <kak.mca@hotmail.com>
2025-12-22 09:49:38 +0530

    Some more leanrings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('angular20');
+export class App implements OnInit{
+  title = signal('angular20');
+  http = inject(HttpClient);
+  members = signal<any>([]);
+
+  ngOnInit(): void {
+    this.getMembers()
+    .then((result)=>{
+      this.members.set(result);
+    }).catch((error)=>{
+      console.log(error);
+    });
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get(`https://localhost:7296/api/members`));      
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
